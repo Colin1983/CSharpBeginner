3fdcc75257cd63fa03101809f50a8d2343a14e53
pkolin <piotrek.kolinski@gmail.com>
2018-03-14 22:26:52 +0100

    The sceond version

--- SmallTestApp/SmallTestApp/Program.cs
+++ SmallTestApp/SmallTestApp/Program.cs
@@ -10,12 +10,22 @@ namespace SmallTestApp
     {
         static void Main(string[] args)
         {
-                Console.WriteLine("Linia1\nLinia2");
-                Console.WriteLine("Wzrost 170\tWaga 65");
-                Console.WriteLine(@"Wzrost 170\tWaga 65");
-                Console.WriteLine("C:\\Windows\\Temp");
-                Console.WriteLine("Tytuł filmu: \"Rój\"");
-                Console.ReadKey();
+            int[][][] tab =
+            {
+                new int[][]
+                {
+                    new int[] {1,2},
+                    new int[] {3,4,5}
+                },
+                new int[][]
+                {
+                    new int[] {6,7},
+                    new int[] {8,9,10}
+                }
+            };
+            Console.WriteLine(tab[0][1][2]);    //wyswietli 5
+            Console.WriteLine(tab[1][0][1]);    //wyswietli 7
+            Console.ReadKey();
         }
     }
 }
