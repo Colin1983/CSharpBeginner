ad31822b7781a7ec13eb3535a6750083d1360396
pkolin <piotrek.kolinski@gmail.com>
2018-03-14 22:20:17 +0100

    Added first version of code

--- SmallTestApp/SmallTestApp/Program.cs
+++ SmallTestApp/SmallTestApp/Program.cs
@@ -10,8 +10,12 @@ namespace SmallTestApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.ReadKey();
+                Console.WriteLine("Linia1\nLinia2");
+                Console.WriteLine("Wzrost 170\tWaga 65");
+                Console.WriteLine(@"Wzrost 170\tWaga 65");
+                Console.WriteLine("C:\\Windows\\Temp");
+                Console.WriteLine("Tytuł filmu: \"Rój\"");
+                Console.ReadKey();
         }
     }
 }
